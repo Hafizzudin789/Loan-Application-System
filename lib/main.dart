bfaae8fa52d80f951792cac1a8f14cd858871d0f
Bidhan Rai <bidhanrai424@gmail.com>
2022-06-14 19:49:31 +0545

    minor ui changes

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget {
             navigatorKey: locator<NavigationService>().navigatorKeyGlobal,
             onGenerateRoute: generateRoute,
             // initialRoute: splashView,
-            initialRoute: layoutView,
+            // initialRoute: layoutView,
           ),
         );
       },
